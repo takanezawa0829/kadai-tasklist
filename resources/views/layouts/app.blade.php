39106c5d295080743e3c6f58b5be13e45030bbb6
takumu.takanezawa <takanen20010829kota@gmail.com>
2020-07-04 19:23:27 +0000

    user login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,16 +20,24 @@
                 <div class="collapse navbar-collapse" id="nav-bar">
                     <ul class="navbar-nav mr-auto"></ul>
                     <ul class="navbar-nav">
+                        @if (Auth::check())
+                            <li class="navbar-text">{{ Auth::user()->name }}</li>
+                        
+                            {{-- タスク作成ページへのリンク --}}
+                            <li>{!! link_to_route('tasks.create', '新規タスクの追加', [], ['class' => 'nav-link']) !!}</li>
+                        
+                            {{-- ログアウトへのリンク --}}
+                            <li>{!! link_to_route('logout.get', 'Logout', [], ['class' => 'nav-link']) !!}</li>
 
-                        {{-- タスク作成ページへのリンク --}}
-                        <li class="nav-item">{!! link_to_route('tasks.create', '新規タスクの追加', [], ['class' => 'nav-link']) !!}</li>
-
-                        {{-- ユーザ登録ページへのリンク --}}
-                        <li>{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
-
-                        {{-- ログインページへのリンク --}}
-                        <li><a href="#">Login</a></li>
-
+                        @else
+                        
+                            {{-- ユーザ登録ページへのリンク --}}
+                            <li>{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
+    
+                            {{-- ログインページへのリンク --}}
+                            <li>{!! link_to_route('login', 'Login', [], ['class' => 'nav-link']) !!}</li>
+                            
+                        @endif
                     </ul>
                 </div>
             </nav>
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -2,30 +2,40 @@
 
 @section('content')
 
-    <h1>タスク一覧</h1>
-
-    @if (count($tasks) > 0)
-        <table class="table table-striped">
-            <tbody>
-                @foreach ($tasks as $task)
-                <tr>
-                    {{-- タスク詳細ページへのリンク --}}
-                    <td>{!! link_to_route('tasks.show', $task->content, ['task' => $task->id]) !!}</td>
-                    <td>{{ $task->status }}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-    @endif
+    @if (Auth::check())
     
-    {{-- タスク作成ページへのリンク --}}
-    {!! link_to_route('tasks.create', '新規タスクの追加', [], ['class' => 'btn btn-primary']) !!}
+        <h1>タスク一覧</h1>
+    
+        @if (count($tasks) > 0)
+            <table class="table table-striped">
+                <tbody>
+                    @foreach ($tasks as $task)
+                    <tr>
+                        {{-- タスク詳細ページへのリンク --}}
+                        <td>{!! link_to_route('tasks.show', $task->content, ['task' => $task->id]) !!}</td>
+                        <td>{{ $task->status }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        @endif
 
-    <div class="center jumbotron">
-        <div class="text-center">
-            <h1>Welcome to the TextLest</h1>
-            {{-- ユーザ登録ページヘのリンク --}}
-            {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+        {{-- タスク作成ページへのリンク --}}
+        {!! link_to_route('tasks.create', '新規タスクの追加', [], ['class' => 'btn btn-primary']) !!}
+
+    @else
+    
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>Welcome to the TextLest</h1>
+                
+                {{-- ユーザ登録ページヘのリンク --}}
+                {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+                
+                {{-- ログインページへのリンク --}}
+                {!! link_to_route('login', 'Login!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+            </div>
         </div>
-    </div>
+        
+    @endif
 @endsection
